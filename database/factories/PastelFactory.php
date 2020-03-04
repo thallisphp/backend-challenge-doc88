4363c076a0da4cb4ae4d7aedca4a3d9bd8e8390c
Thallis <thallisphp@gmail.com>
2020-03-03 23:10:24 -0300

    Testes de pastéis

---
+++ database/factories/PastelFactory.php
@@ -0,0 +1,12 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\Models\Pastel;
+use Faker\Generator as Faker;
+
+$factory->define(Pastel::class, function (Faker $faker) {
+    return [
+        //
+    ];
+});
---
+++ database/seeds/PastelSeeder.php
@@ -0,0 +1,16 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class PastelSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //
+    }
+}
