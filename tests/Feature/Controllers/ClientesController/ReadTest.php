32860a94c400b9bdee35611aac6508ca7990e458
Thallis <thallisphp@gmail.com>
2020-03-03 21:27:22 -0300

    Testes de leitura de clientes

--- tests/Feature/Controllers/ClientesController/ReadTest.php
+++ tests/Feature/Controllers/ClientesController/ReadTest.php
@@ -2,6 +2,7 @@
 
 namespace Tests\Feature\Controllers\ClientesController;
 
+use App\Models\Cliente;
 use Tests\Feature\Controllers\RequiresAuthentication;
 use Tests\TestCase;
 
@@ -26,5 +27,59 @@ class ReadTest extends TestCase {
      */
     private function route( int $id = 0 ) : string {
         return route('api.cliente.show', ['cliente' => $id]);
+    }
+
+    /**
+     * @testdox ID de cliente inválido
+     */
+    public function testInvalidRequest() : void {
+        $this->actingAs($this->user());
+
+        $response = $this->deleteJson($this->route());
+
+        $response->assertNotFound();
+    }
+
+    /**
+     * @testdox ID de cliente deletado
+     */
+    public function testDeleted() : void {
+        $this->actingAs($this->user());
+
+        /** @var Cliente $cliente */
+        $cliente = factory(Cliente::class)->create();
+        $cliente->delete();
+
+        $response = $this->getJson($this->route($cliente->id));
+
+        $response->assertNotFound();
+    }
+
+    /**
+     * @testdox ID de cliente válido
+     */
+    public function testValid() : void {
+        $this->actingAs($this->user());
+
+        /** @var Cliente $cliente */
+        $cliente = factory(Cliente::class)->create();
+
+        $response = $this->getJson($this->route($cliente->id));
+
+        $response->assertOk();
+
+        $response->assertJsonStructure([
+            'nome',
+            'email',
+            'telefone',
+            'data_de_nascimento',
+            'endereco',
+            'complemento',
+            'bairro',
+            'cep',
+            'created_at',
+        ]);
+
+        $response->assertJson($cliente->toArray());
     }
 }
